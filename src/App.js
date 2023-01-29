05e12f30944097f5f03a3c5f800b3f71eef81a04
MayceAlazzawi <maynab4@gmail.com>
2023-01-29 14:27:06 +0300

    Fetching and filtering funcs are working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios, * as others from "axios";
+import Coin from "./Coin";
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+      .then((res) => {
+        setListOfCoins(res.data.coins);
+      });
+  }, []);
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLowerCase());
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader">
+        <input
+          placeHolder="Bitcoin"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => (
+          <Coin key={coin.id} coin={coin} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Coin = (coin) => {
+  return (
+    <div>
+      <h1>Name: {coin.coin.name}</h1>
+      <img src={coin.coin.icon} />
+      <h3>Price: {coin.coin.price} </h3>
+      <h3>Symbol: {coin.coin.symbol} </h3>
+    </div>
+  );
+};
+
+export default Coin;
